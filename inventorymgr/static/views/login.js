3310f1601a0be6a6f5526d6c803ebf80d1449513
Thomas Glamsch <thomas-glamsch@gmx.de>
2019-12-05 09:09:05 +0100

    Login: implement client side.

--- inventorymgr/static/views/login.js
+++ inventorymgr/static/views/login.js
@@ -2,28 +2,71 @@ const template = `
     <div class="columns is-centered">
       <div class="column is-narrow">
         <h1 class="title">Login</h1>
-        <div class="box">
-        <div class="field">
-          <label class="label">Username</label>
-          <div class="control">
-            <input class="input" type="text" placeholder="Username">
+        <div v-if="errorMessage" class="message is-danger">
+          <div class="message-body">
+            {{ errorMessage }}
           </div>
         </div>
-        <div class="field">
-          <label class="label">Password</label>
-          <div class="control">
-            <input class="input" type="password" placeholder="Password">
+        <form class="box" @submit.prevent="sendLoginRequest">
+          <div class="field">
+            <label class="label">Username</label>
+            <div class="control">
+              <input
+                class="input" type="text" placeholder="Username"
+                v-model="username">
+            </div>
           </div>
-        </div>
-        <div class="field">
-          <div class="control">
-            <button class="button is-primary">Login</button>
+          <div class="field">
+            <label class="label">Password</label>
+            <div class="control">
+              <input
+                class="input" type="password" placeholder="Password"
+                v-model="password">
+            </div>
           </div>
-        </div>
-        </div>
+          <div class="field">
+            <div class="control">
+              <button
+                  type="submit"
+                  class="button is-primary">
+                Login
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>`
 
+function sendLoginRequest() {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json;charset=UTF-8');
+    const params = {
+        method: 'POST',
+        headers,
+        body: JSON.stringify({
+            username: this.username,
+            password: this.password
+        })
+    };
+
+    fetch('/api/v1/auth/login', params).then((response) => {
+        if (response.status == 200) {
+            window.is_authenticated = true;
+            this.$router.push('/');
+        } else {
+            response.json().then((json) => {
+                this.errorMessage = json.message;
+            });
+        }
+    })
+}
+
 export default {
-    template
+    template,
+    data: function() {
+        return { username: '', password: '', errorMessage: '' };
+    },
+    methods: {
+        sendLoginRequest
+    }
 }
