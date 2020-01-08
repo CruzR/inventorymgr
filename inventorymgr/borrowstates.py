796f87b78a4f5d54c930e92d74073343e5fb7cb2
Thomas Glamsch <thomas-glamsch@gmx.de>
2020-01-08 09:20:11 +0100

    Enforce user has all required qualifications during checkout.

--- inventorymgr/borrowstates.py
+++ inventorymgr/borrowstates.py
@@ -1,7 +1,7 @@
 """API endpoints for handling borrow states."""
 
 import datetime
-from typing import Any, Dict, Iterable, cast
+from typing import Any, Dict, Iterable, Tuple, cast
 
 from flask import Blueprint, request
 
@@ -10,7 +10,7 @@ from inventorymgr.api.models import (BorrowStateSchema, CheckinRequestSchema,
                                      CheckoutRequestSchema)
 from inventorymgr.auth import authentication_required
 from inventorymgr.db import db
-from inventorymgr.db.models import BorrowState
+from inventorymgr.db.models import BorrowableItem, BorrowState, User
 
 
 bp = Blueprint('borrowstates', __name__, url_prefix='/api/v1/borrowstates')
@@ -31,24 +31,33 @@ def fetch_borrowstates() -> Dict[str, Any]:
 @bp.route('/checkout', methods=('POST',))
 @authentication_required
 @requires_permissions('manage_checkouts')
-def checkout() -> Dict[str, Any]:
+def checkout() -> Tuple[Dict[str, Any], int]:
     """API endpoint to checkout a list of items to a user."""
     checkout_request = CheckoutRequestSchema().load(request.json)
     now = _utcnow()
 
+    borrowing_user = User.query.get(checkout_request['borrowing_user_id'])
+    borrowed_items = [
+        BorrowableItem.query.get(item_id)
+        for item_id in checkout_request['borrowed_item_ids']]
+    unqualified_for = [
+        item for item in borrowed_items
+        if not has_required_qualifications(borrowing_user, item)]
+
+    if unqualified_for:
+        return {
+            'message': 'User is missing qualifications.',
+            'reason': 'missing_qualifications'}, 403
+
     borrowstates = [
-        BorrowState(
-            borrowing_user_id=checkout_request['borrowing_user_id'],
-            borrowed_item_id=item_id,
-            received_at=now)
-        for item_id in checkout_request['borrowed_item_ids']
-    ]
+        BorrowState(borrowing_user=borrowing_user, borrowed_item=item, received_at=now)
+        for item in borrowed_items]
 
     for borrow_state in borrowstates:
         db.session.add(borrow_state)
     db.session.commit()
 
-    return {'borrowstates': BorrowStateSchema(many=True).dump(borrowstates)}
+    return {'borrowstates': BorrowStateSchema(many=True).dump(borrowstates)}, 200
 
 
 @bp.route('/checkin', methods=('POST',))
@@ -72,3 +81,8 @@ def open_borrowstates_for_item(item_id: int) -> Iterable[BorrowState]:
     return cast(
         Iterable[BorrowState],
         BorrowState.query.filter_by(borrowed_item_id=item_id, returned_at=None))
+
+
+def has_required_qualifications(user: User, item: BorrowableItem) -> bool:
+    """Check if user has all required qualifications to borrow item."""
+    return all(q in user.qualifications for q in item.required_qualifications)
--- tests/test_borrowstate.py
+++ tests/test_borrowstate.py
@@ -59,17 +59,26 @@ def test_checkout_insufficient_permissions(client, auth, checkout_request):
     assert response.json['reason'] == 'insufficient_permissions'
 
 
+def test_checkout_missing_qualifications(client, auth, checkout_request):
+    auth.login('test')
+    response = client.post('/api/v1/borrowstates/checkout', json=checkout_request)
+    assert response.status_code == 403
+    assert response.is_json
+    assert response.json['reason'] == 'missing_qualifications'
+
+
 def test_checkout_successful(client, auth, checkout_request, monkeypatch, app):
     def fake_utcnow():
         return datetime.datetime(2020, 1, 4, 13, 37)
     monkeypatch.setattr('inventorymgr.borrowstates._utcnow', fake_utcnow)
     auth.login('test')
+    checkout_request['borrowing_user_id'] = 1
     response = client.post('/api/v1/borrowstates/checkout', json=checkout_request)
     assert response.status_code == 200
     assert response.is_json
     assert response.json['borrowstates'] == [{
         'id': 2,
-        'borrowing_user': {'id': 2, 'username': 'min_permissions_user'},
+        'borrowing_user': {'id': 1, 'username': 'test'},
         'borrowed_item': {'id': 1, 'name': 'existing_item', 'barcode': '0000000000001'},
         'received_at': '2020-01-04T13:37:00',
         'returned_at': None,
