85d0539c8ae0ff844582f19bd62e6800a2bda0cb
Thomas Glamsch <thomas-glamsch@gmx.de>
2020-01-24 08:22:51 +0100

    Add button for creating qualification to qualifications list view.

--- inventorymgr/static/views/qualifications.js
+++ inventorymgr/static/views/qualifications.js
@@ -2,37 +2,45 @@ import { mapState } from '/static/vuex.esm.browser.js'
 
 
 const template = `
-    <table class="table is-fullwidth responsive-table">
-      <thead>
-        <tr>
-          <th>{{ $t('fields.qualification') }}</th>
-          <th>{{ $t('fields.actions') }}</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="qualification in qualifications">
-          <td :data-label="$t('fields.qualification')">{{ qualification.name }}</td>
-          <td :data-label="$t('fields.actions')">
-            <div class="control">
-              <router-link
-                :to="'/qualifications/' + qualification.id + '/edit'"
-                v-slot="{ href, navigate }">
-                <a :href="href" class="button is-primary is-small" @click="navigate">
-                  {{ $t('actions.edit') }}
-                </a>
-              </router-link>
-              <router-link
-                :to="'/qualifications/' + qualification.id"
-                v-slot="{ href, navigate }">
-                <a :href="href" class="button is-primary is-small" @click="navigate">
-                  {{ $t('actions.view') }}
-                </a>
-              </router-link>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>`
+    <div>
+      <table class="table is-fullwidth responsive-table">
+        <thead>
+          <tr>
+            <th>{{ $t('fields.qualification') }}</th>
+            <th>{{ $t('fields.actions') }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="qualification in qualifications">
+            <td :data-label="$t('fields.qualification')">{{ qualification.name }}</td>
+            <td :data-label="$t('fields.actions')">
+              <div class="control">
+                <router-link
+                  :to="'/qualifications/' + qualification.id + '/edit'"
+                  v-slot="{ href, navigate }">
+                  <a :href="href" class="button is-primary is-small" @click="navigate">
+                    {{ $t('actions.edit') }}
+                  </a>
+                </router-link>
+                <router-link
+                  :to="'/qualifications/' + qualification.id"
+                  v-slot="{ href, navigate }">
+                  <a :href="href" class="button is-primary is-small" @click="navigate">
+                    {{ $t('actions.view') }}
+                  </a>
+                </router-link>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <router-link to="/qualifications/new" v-slot="{ href, navigate }">
+        <a class="button" :href="href" @click="navigate">
+          {{ $t('actions.create_qualification') }}
+        </a>
+      </router-link>
+    </div>
+    `
 
 
 export default {
